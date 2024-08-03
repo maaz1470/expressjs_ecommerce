dbd2954ff3b1a0059d6a74fe9b25d069313279de
Rahat Hossain <webmaaz@gmail.com>
2024-08-03 23:12:01 +0600

    some update

--- index.js
+++ index.js
@@ -8,16 +8,20 @@ const port = process.env.PORT || 5000;
 import './Database/Mongoose.js'
 import ProtectedRoutes from './App/ProtectedRoutes.js';
 import dashboardRouter from './routes/panel.js'
+
 app.use(cors({
     origin: 'http://localhost:3000',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    allowedHeaders: ['Content-Type','Authorization']
+    allowedHeaders: [
+        'Content-Type','Authorization'
+    ]
 }))
+// api routes
 app.use(exporess.json())
 app.use('/api',apiRouter)
-app.use('/api/dashboard',ProtectedRoutes, dashboardRouter)
+app.use('/api/panel',ProtectedRoutes, dashboardRouter)
 
-
+// web routes
 app.get('/',(req,res) => {
     return AuthController.home(req, res);
 })
--- routes/panel.js
+++ routes/panel.js
@@ -6,5 +6,11 @@ dashboardRouter.get('/',(req, res) => {
     return res.send('Hello, this is dashboard page')
 })
 
+dashboardRouter.get('/check',(req, res) => {
+    return res.send({
+        status: 200,
+        user: true
+    })
+})
 
 export default dashboardRouter;
